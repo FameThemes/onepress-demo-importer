07c6190b8444c6940e122dedffbff540ec1cddbe
TruongSa <shrimp2t@gmail.com>
2016-02-22 00:06:03 +0700

    Upload to github

---
+++ inc/ft-content.php
@@ -0,0 +1,486 @@
+<?php
+class FT_Demo_Content {
+
+    public $dir;
+    public $url;
+
+
+    /**
+     * XML file to import
+     * Export by WP Exporter
+     * @var string file path
+     */
+    public $xml_file;
+    /**
+     * Customize data file
+     * Data type: serialize string
+     *
+     * Export by plugin https://wordpress.org/plugins/customizer-export-import/ by Justin Busa
+     *
+     * @see https://wordpress.org/plugins/customizer-export-import/
+     * @var string file path
+     */
+    public $customize_file;
+
+    /**
+     * Widget data file
+     *
+     * Data type: JSON string
+     *
+     * Export by plugin  https://wordpress.org/plugins/widget-importer-exporter/ by Steven Gliebe
+     *
+     * @var string file path
+     */
+    public $widget_file;
+
+    /**
+     * Options file
+     * Data type: serialize|json string
+     *
+     * @var string file path
+     */
+    public $option_file;
+
+    /**
+     * Option key to store data in $option_file
+     *
+     * Data type: serialize|json string
+     *
+     * @see update_option
+     * @var string option_key
+     */
+    public $option_key;
+
+
+    function __construct( $options = array() ){
+
+        $options = wp_parse_args( $options, array(
+            'xml' => '',
+            'customize' => '',
+            'widget' => '',
+            'option' => '',
+            'option_key' => '',
+        ));
+
+        $this->xml_file =  $options['xml'];
+        $this->customize_file =  $options['customize'];
+        $this->widget_file =  $options['widget'];
+        $this->option_file =  $options['option'];
+        $this->option_key =  $options['option_key'];
+
+    }
+
+    function import(){
+        $this->import_xml( $this->xml_file );
+        $this->import_customize( $this->customize_file );
+        $this->import_widgets( $this->widget_file );
+        $this->import_widgets( $this->option_key, $this->widget_file );
+        do_action( 'ft_import_after_imported' );
+    }
+
+    function import_options( $option_key, $file ) {
+        if ( ! file_exists( $file ) ) {
+            return false;
+        }
+        if ( ! $option_key ) {
+            return  false;
+        }
+        $data = file_get_contents( $file );
+        $data = json_decode( $data , true );
+
+        update_option( $option_key, $data );
+    }
+
+    /**
+     * Import widget JSON data
+     *
+     * @since 0.4
+     * @global array $wp_registered_sidebars
+     * @param object $data JSON widget data from .wie file
+     * @return array Results array
+     */
+    function import_widgets( $file ) {
+        if ( ! file_exists( $file ) ) {
+            return false;
+        }
+
+        // Get file contents and decode
+        $data = file_get_contents( $file );
+        $data = json_decode( $data , true );
+
+        global $wp_registered_sidebars;
+
+        // Have valid data?
+        // If no data or could not decode
+        if ( empty( $data )  ) {
+           return false;
+        }
+
+        // Hook before import
+
+        // Get all available widgets site supports
+        $available_widgets = wie_available_widgets();
+
+        // Get all existing widget instances
+        $widget_instances = array();
+        foreach ( $available_widgets as $widget_data ) {
+            $widget_instances[$widget_data['id_base']] = get_option( 'widget_' . $widget_data['id_base'] );
+        }
+
+        // Begin results
+        $results = array();
+
+        // Loop import data's sidebars
+        foreach ( $data as $sidebar_id => $widgets ) {
+
+            // Skip inactive widgets
+            // (should not be in export file)
+            if ( 'wp_inactive_widgets' == $sidebar_id ) {
+                continue;
+            }
+
+            // Check if sidebar is available on this site
+            // Otherwise add widgets to inactive, and say so
+            if ( isset( $wp_registered_sidebars[$sidebar_id] ) ) {
+                $sidebar_available = true;
+                $use_sidebar_id = $sidebar_id;
+                $sidebar_message_type = 'success';
+                $sidebar_message = '';
+            } else {
+                $sidebar_available = false;
+                $use_sidebar_id = 'wp_inactive_widgets'; // add to inactive if sidebar does not exist in theme
+                $sidebar_message_type = 'error';
+                $sidebar_message = __( 'Sidebar does not exist in theme (using Inactive)', 'onepress-import' );
+            }
+
+            // Result for sidebar
+            $results[$sidebar_id]['name'] = ! empty( $wp_registered_sidebars[$sidebar_id]['name'] ) ? $wp_registered_sidebars[$sidebar_id]['name'] : $sidebar_id; // sidebar name if theme supports it; otherwise ID
+            $results[$sidebar_id]['message_type'] = $sidebar_message_type;
+            $results[$sidebar_id]['message'] = $sidebar_message;
+            $results[$sidebar_id]['widgets'] = array();
+
+            // Loop widgets
+            foreach ( $widgets as $widget_instance_id => $widget ) {
+
+                $fail = false;
+
+                // Get id_base (remove -# from end) and instance ID number
+                $id_base = preg_replace( '/-[0-9]+$/', '', $widget_instance_id );
+                $instance_id_number = str_replace( $id_base . '-', '', $widget_instance_id );
+
+                // Does site support this widget?
+                if ( ! $fail && ! isset( $available_widgets[$id_base] ) ) {
+                    $fail = true;
+                    $widget_message_type = 'error';
+                    $widget_message = __( 'Site does not support widget', 'onepress-import' ); // explain why widget not imported
+                }
+
+                // Filter to modify settings object before conversion to array and import
+                // Leave this filter here for backwards compatibility with manipulating objects (before conversion to array below)
+                // Ideally the newer wie_widget_settings_array below will be used instead of this
+                $widget = apply_filters( 'wie_widget_settings', $widget ); // object
+
+                // Convert multidimensional objects to multidimensional arrays
+                // Some plugins like Jetpack Widget Visibility store settings as multidimensional arrays
+                // Without this, they are imported as objects and cause fatal error on Widgets page
+                // If this creates problems for plugins that do actually intend settings in objects then may need to consider other approach: https://wordpress.org/support/topic/problem-with-array-of-arrays
+                // It is probably much more likely that arrays are used than objects, however
+                $widget = json_decode( json_encode( $widget ), true );
+
+                // Filter to modify settings array
+                // This is preferred over the older wie_widget_settings filter above
+                // Do before identical check because changes may make it identical to end result (such as URL replacements)
+                $widget = apply_filters( 'wie_widget_settings_array', $widget );
+
+                // Does widget with identical settings already exist in same sidebar?
+                if ( ! $fail && isset( $widget_instances[$id_base] ) ) {
+
+                    // Get existing widgets in this sidebar
+                    $sidebars_widgets = get_option( 'sidebars_widgets' );
+                    $sidebar_widgets = isset( $sidebars_widgets[$use_sidebar_id] ) ? $sidebars_widgets[$use_sidebar_id] : array(); // check Inactive if that's where will go
+
+                    // Loop widgets with ID base
+                    $single_widget_instances = ! empty( $widget_instances[$id_base] ) ? $widget_instances[$id_base] : array();
+                    foreach ( $single_widget_instances as $check_id => $check_widget ) {
+
+                        // Is widget in same sidebar and has identical settings?
+                        if ( in_array( "$id_base-$check_id", $sidebar_widgets ) && (array) $widget == $check_widget ) {
+
+                            $fail = true;
+                            $widget_message_type = 'warning';
+                            $widget_message = __( 'Widget already exists', 'onepress-import' ); // explain why widget not imported
+
+                            break;
+
+                        }
+
+                    }
+
+                }
+
+                // No failure
+                if ( ! $fail ) {
+
+                    // Add widget instance
+                    $single_widget_instances = get_option( 'widget_' . $id_base ); // all instances for that widget ID base, get fresh every time
+                    $single_widget_instances = ! empty( $single_widget_instances ) ? $single_widget_instances : array( '_multiwidget' => 1 ); // start fresh if have to
+                    $single_widget_instances[] = $widget; // add it
+
+                    // Get the key it was given
+                    end( $single_widget_instances );
+                    $new_instance_id_number = key( $single_widget_instances );
+
+                    // If key is 0, make it 1
+                    // When 0, an issue can occur where adding a widget causes data from other widget to load, and the widget doesn't stick (reload wipes it)
+                    if ( '0' === strval( $new_instance_id_number ) ) {
+                        $new_instance_id_number = 1;
+                        $single_widget_instances[$new_instance_id_number] = $single_widget_instances[0];
+                        unset( $single_widget_instances[0] );
+                    }
+
+                    // Move _multiwidget to end of array for uniformity
+                    if ( isset( $single_widget_instances['_multiwidget'] ) ) {
+                        $multiwidget = $single_widget_instances['_multiwidget'];
+                        unset( $single_widget_instances['_multiwidget'] );
+                        $single_widget_instances['_multiwidget'] = $multiwidget;
+                    }
+
+                    // Update option with new widget
+                    update_option( 'widget_' . $id_base, $single_widget_instances );
+
+                    // Assign widget instance to sidebar
+                    $sidebars_widgets = get_option( 'sidebars_widgets' ); // which sidebars have which widgets, get fresh every time
+                    $new_instance_id = $id_base . '-' . $new_instance_id_number; // use ID number from new widget instance
+                    $sidebars_widgets[$use_sidebar_id][] = $new_instance_id; // add new instance to sidebar
+                    update_option( 'sidebars_widgets', $sidebars_widgets ); // save the amended data
+
+                    // After widget import action
+                    $after_widget_import = array(
+                        'sidebar'           => $use_sidebar_id,
+                        'sidebar_old'       => $sidebar_id,
+                        'widget'            => $widget,
+                        'widget_type'       => $id_base,
+                        'widget_id'         => $new_instance_id,
+                        'widget_id_old'     => $widget_instance_id,
+                        'widget_id_num'     => $new_instance_id_number,
+                        'widget_id_num_old' => $instance_id_number
+                    );
+                    do_action( 'wie_after_widget_import', $after_widget_import );
+
+                    // Success message
+                    if ( $sidebar_available ) {
+                        $widget_message_type = 'success';
+                        $widget_message = __( 'Imported', 'onepress-import' );
+                    } else {
+                        $widget_message_type = 'warning';
+                        $widget_message = __( 'Imported to Inactive', 'onepress-import' );
+                    }
+
+                }
+
+                // Result for widget instance
+                $results[$sidebar_id]['widgets'][$widget_instance_id]['name'] = isset( $available_widgets[$id_base]['name'] ) ? $available_widgets[$id_base]['name'] : $id_base; // widget name or ID if name not available (not supported by site)
+                $results[$sidebar_id]['widgets'][$widget_instance_id]['title'] = ! empty( $widget['title'] ) ? $widget['title'] : __( 'No Title', 'onepress-import' ); // show "No Title" if widget instance is untitled
+                $results[$sidebar_id]['widgets'][$widget_instance_id]['message_type'] = $widget_message_type;
+                $results[$sidebar_id]['widgets'][$widget_instance_id]['message'] = $widget_message;
+
+            }
+
+        }
+
+        // Return results
+        return $results;
+
+    }
+
+    function import_customize( $file ) {
+        if ( ! file_exists( $file ) ) {
+            return false ;
+        }
+
+        $raw = file_get_contents( $file );
+        $data = @json_decode( $raw , true );
+        if ( ! is_array( $data ) ) {
+            return false;
+        }
+
+        // Loop through the mods.
+        foreach ( $data as $key => $val ) {
+            // Save the mod.
+            set_theme_mod( $key, $val );
+        }
+
+    }
+
+    function import_xml( $file ){
+        if ( ! defined('WP_LOAD_IMPORTERS') ) {
+            define( 'WP_LOAD_IMPORTERS', true );
+        }
+
+        require_once ABSPATH . 'wp-admin/includes/import.php';
+        $importer_error = false;
+
+        if ( !class_exists( 'WP_Importer' ) ) {
+            $class_wp_importer = ABSPATH . 'wp-admin/includes/class-wp-importer.php';
+            if ( file_exists( $class_wp_importer ) ){
+                require_once($class_wp_importer);
+            } else {
+                $importer_error = true;
+            }
+        }
+
+        if ( !class_exists( 'WP_Import' ) ) {
+            $class_wp_import = dirname( __FILE__ ) .'/importer/wordpress-importer.php';
+            if ( file_exists( $class_wp_import ) ) {
+                require_once($class_wp_import);
+            } else {
+                $importer_error = true;
+            }
+        }
+
+        if($importer_error){
+            die("Error on import");
+        } else {
+
+            if( ! is_file( $file ) ){
+                echo "The XML file containing the dummy content is not available or could not be read .. You might want to try to set the file permission to chmod 755.<br/>If this doesn't work please use the Wordpress importer and import the XML file (should be located in your download .zip: Sample Content folder) manually ";
+            } else {
+
+                $wp_import = new WP_Import();
+                $wp_import->fetch_attachments = true;
+                $wp_import->import( $file );
+                do_action( 'ft_import_after_xml_imported' );
+            }
+        }
+    }
+
+    /**
+     * Available widgets
+     *
+     * Gather site's widgets into array with ID base, name, etc.
+     * Used by export and import functions.
+     *
+     * @since 0.4
+     * @global array $wp_registered_widget_updates
+     * @return array Widget information
+     */
+    static function get_available_widgets() {
+
+        global $wp_registered_widget_controls;
+
+        $widget_controls = $wp_registered_widget_controls;
+
+        $available_widgets = array();
+
+        foreach ( $widget_controls as $widget ) {
+
+            if ( ! empty( $widget['id_base'] ) && ! isset( $available_widgets[$widget['id_base']] ) ) { // no dupes
+
+                $available_widgets[$widget['id_base']]['id_base'] = $widget['id_base'];
+                $available_widgets[$widget['id_base']]['name'] = $widget['name'];
+
+            }
+
+        }
+
+        return $available_widgets;
+
+    }
+
+
+    /**
+     * Generate export data
+     *
+     * @since 0.1
+     * @return string Export file contents
+     */
+    static function generate_widgets_export_data() {
+
+        // Get all available widgets site supports
+        $available_widgets = self::get_available_widgets();
+
+        // Get all widget instances for each widget
+        $widget_instances = array();
+        foreach ( $available_widgets as $widget_data ) {
+
+            // Get all instances for this ID base
+            $instances = get_option( 'widget_' . $widget_data['id_base'] );
+
+            // Have instances
+            if ( ! empty( $instances ) ) {
+
+                // Loop instances
+                foreach ( $instances as $instance_id => $instance_data ) {
+
+                    // Key is ID (not _multiwidget)
+                    if ( is_numeric( $instance_id ) ) {
+                        $unique_instance_id = $widget_data['id_base'] . '-' . $instance_id;
+                        $widget_instances[$unique_instance_id] = $instance_data;
+                    }
+
+                }
+
+            }
+
+        }
+
+        // Gather sidebars with their widget instances
+        $sidebars_widgets = get_option( 'sidebars_widgets' ); // get sidebars and their unique widgets IDs
+        $sidebars_widget_instances = array();
+        foreach ( $sidebars_widgets as $sidebar_id => $widget_ids ) {
+
+            // Skip inactive widgets
+            if ( 'wp_inactive_widgets' == $sidebar_id ) {
+                continue;
+            }
+
+            // Skip if no data or not an array (array_version)
+            if ( ! is_array( $widget_ids ) || empty( $widget_ids ) ) {
+                continue;
+            }
+
+            // Loop widget IDs for this sidebar
+            foreach ( $widget_ids as $widget_id ) {
+
+                // Is there an instance for this widget ID?
+                if ( isset( $widget_instances[$widget_id] ) ) {
+
+                    // Add to array
+                    $sidebars_widget_instances[$sidebar_id][$widget_id] = $widget_instances[$widget_id];
+
+                }
+
+            }
+
+        }
+
+        // Filter pre-encoded data
+        $data = apply_filters( 'ft_widget_unencoded_export_data', $sidebars_widget_instances );
+
+        // Encode the data for file contents
+        if ( ! $data ) {
+            return '';
+        }
+        $encoded_data = json_encode( $data );
+
+        // Return contents
+        return apply_filters( 'ft_widget_generate_export_data', $encoded_data );
+
+    }
+
+    static function generate_theme_mods_export_data(){
+        $data = get_theme_mods();
+        if ( ! $data ) {
+            return '';
+        }
+        return json_encode( get_theme_mods() ) ;
+    }
+    static function generate_options_export_data( $option_key  ){
+        $options = get_option( $option_key , array() );
+        if ( ! $options ) {
+            return '';
+        }
+        return json_encode( $options ) ;
+    }
+
+}
---
+++ onepress-importer.php
@@ -0,0 +1,211 @@
+<?php
+/*
+Plugin Name: OnePress Demo Importer
+Plugin URI: http://www.famethemes.com/
+Description: The OnePress Plus plugin adds powerful premium features to OnePress theme.
+Author: famethemes
+Author URI:  http://www.famethemes.com/
+Version: 1.0.3
+Text Domain: onepress-importer
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+
+class OnePress_Demo_Import {
+    public $dir;
+    public $url;
+    function __construct( ){
+
+        $this->url = trailingslashit( plugins_url('', __FILE__) );
+        $this->dir = trailingslashit( plugin_dir_path( __FILE__) );
+
+        require_once $this->dir.'inc/ft-content.php';
+
+        add_action( 'wp_ajax_ft_demo_import_content', array( $this, 'ajax_import' ) );
+
+        add_action( 'onepress_admin_more_tabs', array( $this, 'add_tab' ), 66 );
+        add_action( 'onepress_more_tabs_details', array( $this, 'tab_details' ), 66 );
+        add_action( 'ft_import_after_imported', array( $this, 'setup_demo' ), 66 );
+    }
+
+    function ajax_import(){
+        $nonce = $_REQUEST['_nonce'];
+        if ( ! wp_verify_nonce( $nonce, 'ft_demo_import' ) ) {
+            die( 'Security check' );
+        } else {
+            // Do stuff here.
+        }
+
+        $import = new FT_Demo_Content(array(
+            'xml' => $this->dir.'data/dummy-data.xml',
+            'customize' => $this->dir.'data/customize.json',
+            'widget' => $this->dir.'data/widgets.json',
+            'option' => '',
+            'option_key' => '',
+        ) );
+
+        $import->import();
+
+        update_option( 'ft_demo_imported', 1 );
+
+        die( 'done' );
+    }
+    function setup_demo(){
+
+        // Try to set home page
+        $page = get_page_by_title( 'Home' );
+        if ( get_post_type( $page ) == 'page' ) {
+            echo 'is_home';
+            update_option( 'show_on_front', 'page' );
+            update_option( 'page_on_front', $page->ID );
+
+            // Try to set blog page
+            $blog_page = get_page_by_title( 'News' );
+            if ( get_post_type( $page ) == 'page' ) {
+                update_option('page_for_posts', $blog_page->ID);
+            }
+        }
+
+        // Setup demo menu
+        $menu = get_term_by( 'name', 'Primary', 'nav_menu' );
+        if ( $menu ) {
+            // Getting new theme's theme location settings
+            $nav_menu_locations = get_theme_mod( 'nav_menu_locations' );
+            $nav_menu_locations[ 'primary' ] = $menu->term_id;
+            set_theme_mod( 'nav_menu_locations', $nav_menu_locations );
+        }
+
+    }
+
+    function add_tab(){
+        // Check for current viewing tab
+        $tab = null;
+        if ( isset( $_GET['tab'] ) ) {
+            $tab = $_GET['tab'];
+        } else {
+            $tab = null;
+        }
+        ?>
+        <a href="?page=ft_onepress&tab=demo_content" class="nav-tab<?php echo $tab == 'demo_content' ? ' nav-tab-active' : null; ?>"><?php esc_html_e( 'Demo Content', 'onepress-import' ); ?></a>
+        <?php
+    }
+
+    function tab_details( $details ){
+        $tab = null;
+        if ( isset( $_GET['tab'] ) ) {
+            $tab = $_GET['tab'];
+        } else {
+            $tab = null;
+        }
+
+        if ( $tab != 'demo_content' ) {
+            return ;
+        }
+
+        $show_export = false;
+        if ( isset( $_REQUEST['export'] ) && $_REQUEST['export'] == 1 ) {
+            $show_export = true;
+        }
+
+        ?>
+
+        <div class="theme_info info-tab-content">
+            <?php if ( $show_export ) { ?>
+            <?php /*
+            <div style="margin-bottom: 25px;">
+                <h3>Options Export</h3>
+                <textarea readonly="true" style="width: 100%;" rows="10"><?php echo esc_attr( stripslashes_deep( FT_Demo_Content::generate_options_export_data( 'sidebars_widgets' ) ) );  ?></textarea>
+            </div>
+            */ ?>
+
+            <div style="margin-bottom: 25px;">
+                <h3>Widget Export</h3>
+                <textarea readonly="true" style="width: 100%;" rows="10"><?php echo esc_textarea( FT_Demo_Content::generate_widgets_export_data() );  ?></textarea>
+            </div>
+
+            <div style="margin-bottom: 25px;">
+                <h3>Customize Export</h3>
+                <textarea readonly="true" style="width: 100%;" rows="10"><?php echo esc_textarea( FT_Demo_Content::generate_theme_mods_export_data( ) );  ?></textarea>
+            </div>
+            <?php } ?>
+
+            <div class="action-required">
+                <p class="tie_message_hint">Importing demo data (post, pages, images, theme settings, ...) is the easiest way to setup your theme. It will
+                    allow you to quickly edit everything instead of creating content from scratch. When you import the data following things will happen:</p>
+
+                <ul style="padding-left: 20px;list-style-position: inside;list-style-type: square;}">
+                    <li>No existing posts, pages, categories, images, custom post types or any other data will be deleted or modified .</li>
+                    <li>No WordPress settings will be modified .</li>
+                    <li>Posts, pages, some images, some widgets and menus will get imported .</li>
+                    <li>Images will be downloaded from our server, these images are copyrighted and are for demo use only .</li>
+                    <li>Please click import only once and wait, it can take a couple of minutes</li>
+                </ul>
+            </div>
+
+            <div class="action-required"><p class="tie_message_hint">Before you begin, make sure all the required plugins are activated.</p></div>
+            <?php if ( get_option( 'ft_demo_imported' ) == 1 ) { ?>
+                <div class="action-required" style="border-left-color: #a1d3a2; clear:both;">
+                    <p><?php _e('Demo already imported', 'radium'); ?></p>
+                </div>
+            <?php } ?>
+
+            <p>
+                <a href="#" class="ft_demo_import button-primary" name="demo_import" /><?php
+
+                if( get_option( 'ft_demo_imported' ) == 1 ) {
+                    _e('Import Again', 'onepress-import');
+                } else {
+                    _e('Import Demo Data', 'onepress-import');
+                }
+
+                ?></a>
+                <span class="spinner" style="float: none; margin-left: 0px; margin-top: -2px;"></span>
+            </p>
+
+        </div>
+        <script type="text/javascript">
+            jQuery( document).ready( function( $ ){
+                $( '.ft_demo_import').on( 'click', function( e ){
+                    e.preventDefault();
+                    var btn = $(this);
+                    if ( btn.hasClass( 'disabled' ) ) {
+                        return false;
+                    }
+                    //var c = confirm( "<?php echo esc_attr( 'Are you sure want to import demo content ?' ); ?>" );
+                    var c = true;
+                    if ( c ) {
+
+                        btn.addClass('disabled');
+
+                        $('.spinner', btn.parent() ).css('visibility', 'visible');
+
+                        var params = {
+                            'action': 'ft_demo_import_content',
+                            '_nonce': '<?php echo wp_create_nonce( 'ft_demo_import' ); ?>'
+                        };
+
+                        $.post( window.ajaxurl, params, function ($data) {
+                            btn.removeClass('disabled');
+                            $('.spinner', btn.parent() ).css('visibility', 'hidden');
+
+                            window.location = '<?php echo admin_url( 'themes.php?page=ft_onepress&tab=demo_content&imported=1' ); ?>';
+
+                        });
+                    }
+
+                } );
+            } );
+        </script>
+        <?php
+    }
+
+
+
+
+}
+
+ //remove_theme_mods();
+
+if ( is_admin() ) {
+    new OnePress_Demo_Import();
+}
